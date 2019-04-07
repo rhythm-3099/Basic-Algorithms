ab6abe7bfaaa3fa232d282e5f491e4ab3276f5e6
Rhythm Patel <36991377+rhythm-3099@users.noreply.github.com>
2019-04-07 18:11:08 +0530

    Cycle detection using DSU simple

---
+++ cycleDetectionUsingDSUSimple.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<string.h>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+#include<stack>
+#define lli long long int
+#define db double
+#define str string
+#define ch char
+#define pb push_back
+#define FOR(a,n) for(i=0;i<n;i++){	cin>>a[i];}
+#define sortVector(a) sort(a.begin(),a.end())
+#define reverseSortVector(a) sort(a.rbegin(),a.rend())
+#define FAST ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);srand(time(NULL));
+#define endl "\n"
+using namespace std;
+
+// This is the DSU algorithm with balanced union function. It maintains the "size" array and establishes the root element comparing the size of the sets.
+// This program prints "YES" if the undirected graph contains a cycle, prints "NO" otherwise.
+// Note: this does not use path compression
+// Note: vectors "arr" and "size" should be declared globally
+
+vector<lli>arr(1000007);    // represents the parent node of every node
+vector<lli>size(1000007);   // represents the size of the subsets
+
+lli root(lli a){
+    while(arr[a]!=a){
+        a=arr[a];
+    }
+    return a;
+}
+
+bool find(lli a, lli b){
+    //cout << "a " << a << " root a " << root(arr,a) << " b " << b << " root b " << root(arr,b) << endl;
+    if(root(a)==root(b)){   //if A and B have same root, that means that they are connected
+        return true;
+    } else {                // they aren't connected
+        return false;
+    }
+}
+
+void weightedUnion(lli a, lli b){  // this merges the subsets containing a and b
+    lli rootOfA=root(a);
+    lli rootOfB=root(b);
+    cout << "a " << a << " roota " << root(a)<< " arr[roota] " << arr[rootOfA] << " b " << b << " rootb " << root(b) << " arr[rootB] " << arr[rootOfB] << endl;
+    if(size[rootOfA]<size[rootOfB]){    // make B as a parent
+        arr[rootOfA]=arr[rootOfB];
+        size[rootOfB]+=size[rootOfA];
+    } else {
+        arr[rootOfB]=arr[rootOfA];
+        size[rootOfA]+=size[rootOfB];
+    }
+    cout << "a " << a << " roota " << root(a)<< " arr[roota] " << arr[rootOfA] << " b " << b << " rootb " << root(b) << " arr[rootB] " << arr[rootOfB] << endl;
+}
+
+int main() {
+    //FAST;
+    lli i,j,k,n,m;
+    cout << "Enter the number of nodes and edges:\n";
+    cin>>n>>m; // n=nodes, m=edges
+    for(i=0;i<n;i++)
+        arr[i]=i;
+    for(i=0;i<=n;i++){
+        size[i]=1;
+    }
+    vector<pair<lli,lli> > adj(m+1);
+    cout << "Enter the starting node and ending node of each edge:\n";
+    for(i=0;i<m;i++){
+        cin>>adj[i].first>>adj[i].second;
+    }
+    for(i=0;i<m;i++){
+        if(!find(adj[i].first,adj[i].second)){
+            weightedUnion(adj[i].first,adj[i].second);
+        } else {
+            cout << "Cycle detected!\n";
+            return 0;
+        }
+    }
+    cout << "No cycle in the graph!\n";
+	return 0;
+}
