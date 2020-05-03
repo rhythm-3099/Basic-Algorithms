aa11f5e8cbe4e8458a380ae133effaf1fd133073
Rhythm Patel <36991377+rhythm-3099@users.noreply.github.com>
2020-05-03 15:42:21 +0530

    fix indentation

--- myBfs.cpp
+++ myBfs.cpp
@@ -35,29 +35,29 @@ void extendedEuclid(lli A, lli B)	// don't consider this!
 /*    We use Queue DataStructure for the implementation of BFS Algorithm  */
 
 int main() {
-	lli i,j,k,n,m,a,b,root;
-	queue<lli> q;
-	cout << "The number of nodes: \n";
-	cin >> n;   // n = Number of nodes, m = Number of Edges
-	cout << "\nThe number of edges: \n";
-	cin >> m;
-	vector< vector<lli> > adj(n+1);  //  To show the connection between two nodes by an edge
-	bool visited[n+1];  // This boolean array will help in knowing if all the nodes are visited or not
-	memset(visited,false,sizeof(visited));
+		lli i,j,k,n,m,a,b,root;
+		queue<lli> q;
+		cout << "The number of nodes: \n";
+		cin >> n;   // n = Number of nodes, m = Number of Edges
+		cout << "\nThe number of edges: \n";
+		cin >> m;
+		vector< vector<lli> > adj(n+1);  //  To show the connection between two nodes by an edge
+		bool visited[n+1];  // This boolean array will help in knowing if all the nodes are visited or not
+		memset(visited,false,sizeof(visited));
 
-	/*Input the edges between two nodes  */
-	for(i=0;i<m;i++){
-		cout << "Enter the nodes between whom an edge is present \n";
-		cin >> a >> b;
-		adj[a].pb(b);  // Push back the value of b in adj[a]
-		adj[b].pb(a);  // Push back the value of a in adj[b]
-	}
-	root = 1;  // Root is the starting node. Here, the starting node is 1
-    visited[root]=true;  //  Root node is visited
-    q.push(root);
-    cout << "\nThe order in which the graph is traversed is:\n";
+		/*Input the edges between two nodes  */
+		for(i=0;i<m;i++){
+			cout << "Enter the nodes between whom an edge is present \n";
+			cin >> a >> b;
+			adj[a].pb(b);  // Push back the value of b in adj[a]
+			adj[b].pb(a);  // Push back the value of a in adj[b]
+		}
+		root = 1;  // Root is the starting node. Here, the starting node is 1
+    	visited[root]=true;  //  Root node is visited
+    	q.push(root);
+    	cout << "\nThe order in which the graph is traversed is:\n";
 
-    while(!q.empty()){
+    	while(!q.empty()){
         k = q.front();   // We will traverse from this node
         q.pop();
         cout << k << " "; // This will print the order in which the graph is traversed by BFS
